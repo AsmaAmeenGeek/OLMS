efd17b850a12da21ff1a08c6af968413f95f6eb1
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-01-02 20:37:34 +0530

    change footer margin

--- user/books_details.php
+++ user/books_details.php
@@ -212,7 +212,7 @@ if (isset($_GET['BookId'])) {
                 </div>
             </footer>
 
-            <p style="margin-left: 650px; margin-top: 20px;">&copy; 2024 Million Library. All rights reserved.</p>
+            <p style="margin-left: 690px; margin-top: 20px;">&copy; 2024 Million Library. All rights reserved.</p>
 
             <script src="script.js"></script>
         </body>
--- user/message.php
+++ user/message.php
@@ -179,7 +179,7 @@ if ($_SESSION['RollNo']) {
             </div>
         </footer>
 
-        <p style="margin-left: 650px; margin-top: 20px;">&copy; 2024 Million Library. All rights reserved.</p>
+        <p style="margin-left: 690px; margin-top: 20px;">&copy; 2024 Million Library. All rights reserved.</p>
 
         <script src="script.js"></script>
     </body>
