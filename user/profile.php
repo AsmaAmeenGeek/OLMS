a09b65657ab9392e8c2b4de26410bfa534b653ea
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-02-25 06:47:52 +0530

    change extension

--- user/profile.php
+++ user/profile.php
@@ -45,7 +45,7 @@ ob_end_clean();
         <div class="menu_title menu_dahsboard"></div>
         <!-- start -->
         <li class="item">
-          <a href="home.html" class="nav_link submenu_item">
+          <a href="home.php" class="nav_link submenu_item">
             <span class="navlink_icon">
               <i class="bx bx-home-alt"></i>
             </span>
@@ -90,7 +90,7 @@ ob_end_clean();
         </li>
 
         <li class="item">
-          <a href="currently_reserved.html" class="nav_link submenu_item">
+          <a href="currently_reserved.php" class="nav_link submenu_item">
             <span class="navlink_icon">
               <i class='bx bxs-edit'></i>
             </span>
@@ -180,20 +180,20 @@ ob_end_clean();
         <p>OLMS</p>
       </div>
       <div>
-        <ul>
-          <li><a href="Help.html">About Us</a></li>
-          <li><a href="Help.html">Contact Us</a></li>
-          <li><a href="Help.html">Terms and conditions</a></li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li><a href="Help.html">Plans</a></li>
-          <li><a href="Help.html">FAQs</a></li>
-          <li><a href="Help.html">Help</a></li>
-        </ul>
-      </div>
-    </div>
+                <ul>
+                    <li><a href="Help.php">About Us</a></li>
+                    <li><a href="Help.php">Contact Us</a></li>
+                    <li><a href="Help.php">Terms and conditions</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="Help.php">Plans</a></li>
+                    <li><a href="Help.php">FAQs</a></li>
+                    <li><a href="Help.php">Help</a></li>
+                </ul>
+            </div>
+   
   </footer>
 
   <p style="margin-left: 690px; margin-top: 20px;">&copy; 2024 Million Library. All rights reserved.</p>
