ca7e5b49dc168fe77e2163e12efa7a2c321dc72b
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-02-27 19:03:59 +0530

    change some queries

--- OLMS/user.sql
+++ OLMS/user.sql
@@ -2,9 +2,9 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Dec 27, 2024 at 07:23 AM
--- Server version: 10.4.32-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2025 at 02:32 PM
+-- Server version: 8.0.23
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,30 +28,31 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `user` (
-  `RollNo` varchar(250) NOT NULL,
-  `Name` varchar(250) DEFAULT NULL,
-  `Type` varchar(20) DEFAULT NULL,
-  `Category` varchar(50) DEFAULT NULL,
-  `EmailId` varchar(250) DEFAULT NULL,
-  `MobNo` bigint(11) DEFAULT NULL,
-  `Password` varchar(250) DEFAULT NULL
+  `RollNo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Name` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Type` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Category` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `EmailId` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `MobNo` bigint DEFAULT NULL,
+  `Password` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ProfilePicture` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`RollNo`, `Name`, `Type`, `Category`, `EmailId`, `MobNo`, `Password`) VALUES
-('ADMIN', 'Admin', 'Admin', '', 'admin@gmail.com', 701237865, 'admin'),
-('s001', 'Shiva', 'Student', 'ST', 'shiva@gmail.com', 703237865, 's123'),
-('s002', 'John', 'Student', 'GEN', 'john@gmail.com', 701234865, 's223'),
-('s003', 'Karthi', 'Student', 'OBC', 'karthi@gmail.com', 701297865, 's323'),
-('s004', 'Moni', 'Student', 'GEN', 'moni@gmail.com', 721237865, 's423'),
-('s005', 'Shakthi', 'Student', 'ST', 'shakthi@gmail.com', 701237065, 's523'),
-('s006', 'Rachel', 'Student', 'OBC', 'rachel@gmail.com', 701231865, 's623'),
-('s007', 'Adam', 'Student', 'GEN', 'adam@gmail.com', 771237865, 's723'),
-('s008', 'Kevin', 'Student', 'GEN', 'kevin@gmail.com', 701237868, 's823'),
-('s009', 'Bob', 'Student', 'GEN', 'bob@gmail.com', 70123783, 's923');
+INSERT INTO `user` (`RollNo`, `Name`, `Type`, `Category`, `EmailId`, `MobNo`, `Password`, `ProfilePicture`) VALUES
+('ADMIN', 'Admin', 'Admin', '', 'admin@gmail.com', 701237865, 'admin', NULL),
+('s001', 'Shiva', 'Student', 'GEN', 'shiva@gmail.com', 703237860, 's123', '../Assets/profile/profile_3.png'),
+('s002', 'John', 'Student', 'GEN', 'john@gmail.com', 701234865, 's223', '../Assets/profile/profile_8.jpg'),
+('s003', 'Karthi', 'Student', 'OBC', 'karthi@gmail.com', 701297865, 's323', '../Assets/profile/profile_1.jpg'),
+('s004', 'Moni', 'Student', 'GEN', 'moni@gmail.com', 721237865, 's423', '../Assets/profile/profile_7.jpg'),
+('s005', 'Shakthi', 'Student', 'ST', 'shakthi@gmail.com', 701237065, 's523', '../Assets/profile/profile_6.jpg'),
+('s006', 'Rachel', 'Student', 'OBC', 'rachel@gmail.com', 701231865, 's623', '../Assets/profile/profile_5.jpg'),
+('s007', 'Adam', 'Student', 'GEN', 'adam@gmail.com', 771237865, 's723', '../Assets/profile/profile_9.png'),
+('s008', 'Kevin', 'Student', 'GEN', 'kevin@gmail.com', 701237868, 's823', '../Assets/profile/profile_10.jpg'),
+('s009', 'Bob', 'Student', 'GEN', 'bob@gmail.com', 70123783, 's923', '../Assets/profile/profile_2.png');
 
 --
 -- Indexes for dumped tables
