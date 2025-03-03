a3bff0f2befb140a399e908dbaddbd7bbe8a11f3
Asma Ameen <114100671+AsmaAmeenGeek@users.noreply.github.com>
2025-03-03 12:14:12 +0530

    Update link

--- admin/addBook.php
+++ admin/addBook.php
@@ -51,7 +51,7 @@ if ($_SESSION['RollNo']) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
     <title>OLMS</title>
     <link rel="stylesheet" href="style.css" />
 </head>
