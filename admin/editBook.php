18f92e230827d6eb0e4f690c37209d45ac067627
Asma Ameen <114100671+AsmaAmeenGeek@users.noreply.github.com>
2025-03-04 15:48:04 +0530

    Update page titles

--- admin/editBook.php
+++ admin/editBook.php
@@ -30,7 +30,7 @@ if ($userResult && $userResult->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Boxicons CSS -->
     <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
-    <title>OLMS</title>
+    <title>Edit Book Details</title>
     <link rel="stylesheet" href="style.css" />
   </head>
 
