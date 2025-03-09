97b55b78581626cc1b76b4c855c6a26a343d83e4
Asma Ameen <114100671+AsmaAmeenGeek@users.noreply.github.com>
2025-03-09 21:54:08 +0530

    Update page title

--- admin/return_request.php
+++ admin/return_request.php
@@ -50,7 +50,7 @@ $ProfilePicture = ($userResult->num_rows > 0) ? $userResult->fetch_assoc()['Prof
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Boxicons CSS -->
     <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
-    <title>return</title>
+    <title>Return Requests</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
