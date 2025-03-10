37af485d7e50900a465f1700a045dbcd0ca0b4c9
Asma Ameen <114100671+AsmaAmeenGeek@users.noreply.github.com>
2025-03-10 20:21:13 +0530

    Implemented profile dropdown UI and function

--- admin/script.js
+++ admin/script.js
@@ -99,3 +99,15 @@ function cancelEdit(messageId, originalText) {
   document.getElementById("message_" + messageId).innerText = originalText;
 }
 
+// profile pic dropdown
+document.getElementById('profilePic').addEventListener('click', function() {
+  var dropdown = document.getElementById('profileDropdown');
+  dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+});
+document.addEventListener('click', function(event) {
+  var dropdown = document.getElementById('profileDropdown');
+  var profilePic = document.getElementById('profilePic');
+  if (!profilePic.contains(event.target) && !dropdown.contains(event.target)) {
+      dropdown.style.display = 'none';
+  }
+});
